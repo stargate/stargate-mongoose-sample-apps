6389ce5a98a4b6116e77d15ad16e493e63ba2dde
Kathiresan Selvaraj <kathiresan.selvaraj@datastax.com>
2023-08-21 10:13:24 -0400

    Removed AstraEnvironment option

--- netlify-functions-ecommerce/.config/astra.js
+++ netlify-functions-ecommerce/.config/astra.js
@@ -3,7 +3,7 @@
 const {createAstraUri, AstraEnvironment} = require("stargate-mongoose");
 
 module.exports = Object.freeze({
-  jsonApiUrl: createAstraUri(process.env.ASTRA_DBID, process.env.ASTRA_REGION, process.env.ASTRA_KEYSPACE, process.env.ASTRA_APPLICATION_TOKEN, process.env.ASTRA_ENVIRONMENT ? process.env.ASTRA_ENVIRONMENT : AstraEnvironment.PRODUCTION),
+  jsonApiUrl: createAstraUri(process.env.ASTRA_DBID, process.env.ASTRA_REGION, process.env.ASTRA_KEYSPACE, process.env.ASTRA_APPLICATION_TOKEN),
   jsonApiConnectOptions: {
     isAstra: true
   },
