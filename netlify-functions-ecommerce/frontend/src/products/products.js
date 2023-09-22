f075c8217340b703c3533a377516370a773df7c0
Valeri Karpov <val@karpov.io>
2023-09-22 17:21:51 -0400

    fix some issues that popped up with astra testing: correctly set cart key in localStorage, add loading spinners

--- netlify-functions-ecommerce/frontend/src/products/products.js
+++ netlify-functions-ecommerce/frontend/src/products/products.js
@@ -4,11 +4,15 @@ const BaseComponent = require('../BaseComponent');
 
 module.exports = app => app.component('products', {
   inject: ['state'],
+  data: () => ({
+    submitting: null
+  }),
   methods: {
     formatPrice(price) {
       return `$${price.toFixed(2)}`;
     },
     async addToCart(product) {
+      this.submitting = product;
       const body = {
         items: [{ productId: product._id, quantity: 1 }]
       };
@@ -23,10 +27,11 @@ module.exports = app => app.component('products', {
         body: JSON.stringify(body)
       }).then(res => res.json());
       this.state.cart = res;
-      if (!this.state.cartId) {
+      if (!this.state.cartId || this.state.cartId !== res._id) {
         this.state.cartId = res._id;
         window.localStorage.setItem('__cartKey', res._id);
       }
+      this.submitting = null;
     }
   },
   extends: BaseComponent(require('./products.html'), require('./products.css'))
