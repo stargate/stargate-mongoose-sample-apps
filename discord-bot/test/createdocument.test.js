c1847b1c7b1fe389ad0e1f1023719336a4e35ce1
Valeri Karpov <val@karpov.io>
2023-11-22 17:34:17 -0500

    test cases for discord bot

---
+++ discord-bot/test/createdocument.test.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const Bot = require('../models/bot');
+const assert = require('assert');
+const { describe, it } = require('mocha');
+const createdocument = require('../commands/createdocument');
+const sinon = require('sinon');
+
+describe('createdocument', function() {
+  it('inserts a new document', async function() {
+    await Bot.deleteMany({});
+
+    const interaction = {
+      reply: sinon.stub()
+    };
+    await createdocument.execute(interaction);
+    assert.ok(interaction.reply.calledOnce);
+    assert.deepEqual(interaction.reply.getCalls()[0].args, ['done!']);
+
+    const docs = await Bot.find();
+    assert.equal(docs.length, 1);
+    assert.equal(docs[0].name, 'I am a document');
+  });
+});
--- discord-bot/test/setup.js
+++ discord-bot/test/setup.js
@@ -1,3 +1,21 @@
 'use strict';
 
 require('dotenv').config({ path: `${__dirname}/../.env.test` });
+
+const Bot = require('../models/bot');
+const { before } = require('mocha');
+const mongoose = require('../mongoose');
+
+const uri = process.env.JSON_API_URL;
+const jsonApiConnectOptions = {
+  username: process.env.JSON_API_AUTH_USERNAME,
+  password: process.env.JSON_API_AUTH_PASSWORD,
+  authUrl: process.env.JSON_API_AUTH_URL
+};
+
+before(async function() {
+  console.log('Connecting to', uri);
+  await mongoose.connect(uri, jsonApiConnectOptions);
+  await Bot.db.dropCollection('bots').catch(() => {});
+  await Bot.createCollection();
+});
