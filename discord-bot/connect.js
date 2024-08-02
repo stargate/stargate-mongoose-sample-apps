f3b9015b4fdc3245136b28f93afd91fd9d129fa9
Valeri Karpov <val@karpov.io>
2024-08-02 13:19:10 -0400

    remove unnecessary AUTH_URL param from .env examples and connect()

--- discord-bot/connect.js
+++ discord-bot/connect.js
@@ -19,8 +19,7 @@ module.exports = async function connect() {
     uri = process.env.DATA_API_URI;
     jsonApiConnectOptions = {
       username: process.env.DATA_API_AUTH_USERNAME,
-      password: process.env.DATA_API_AUTH_PASSWORD,
-      authUrl: process.env.DATA_API_AUTH_URL
+      password: process.env.DATA_API_AUTH_PASSWORD
     };
   }
   console.log('Connecting to', uri);
