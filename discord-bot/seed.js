e5decd2e4c4c1741f8230eec70bd0efdc28b8f24
Valeri Karpov <val@karpov.io>
2024-08-05 11:23:28 -0400

    handle listCollections return value correctly

--- discord-bot/seed.js
+++ discord-bot/seed.js
@@ -14,7 +14,8 @@ seed().catch(err => {
 async function seed() {
   await connect();
   
-  const existingCollections = await mongoose.connection.listCollections();
+  const existingCollections = await mongoose.connection.listCollections()
+    .then(collections => collections.map(c => c.name));;
   if (!existingCollections.includes(Bot.collection.collectionName)) {
     await Bot.createCollection();
   }
