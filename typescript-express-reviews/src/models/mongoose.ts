9d39399fb4c52f572c865d0abf0c641a05716c27
Valeri Karpov <val@karpov.io>
2026-06-30 12:17:30 -0400

    mongoose studio with tables

--- typescript-express-reviews/src/models/mongoose.ts
+++ typescript-express-reviews/src/models/mongoose.ts
@@ -5,4 +5,16 @@ mongoose.set('autoIndex', false);
 
 import { driver } from '@datastax/astra-mongoose';
 
+if (process.env.DATA_API_TABLES) {
+  // Mongoose Studio relies on countDocuments and estimatedDocumentCount, so need
+  // to add those to show count results.
+  mongoose.plugin(schema => {
+    schema.pre(['countDocuments', 'estimatedDocumentCount'], async function (this: any) {
+      throw mongoose.skipMiddlewareFunction(
+        await this.model.find().select({ _id: 1 }).then((res: any) => res.length)
+      );
+    });
+  });
+}
+
 export default mongoose.setDriver(driver);
