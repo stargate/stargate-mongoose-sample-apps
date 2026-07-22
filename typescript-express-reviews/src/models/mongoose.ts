84a2941542a030ba13a4d6f91b182684e702acb1
Valeri Karpov <val@karpov.io>
2026-07-22 10:58:45 -0400

    working Mongoose Studio with tables and new astra-mongoose version

--- typescript-express-reviews/src/models/mongoose.ts
+++ typescript-express-reviews/src/models/mongoose.ts
@@ -1,43 +1,12 @@
 import mongoose from 'mongoose';
+import setupMongooseStudioUserModels from '../setupMongooseStudioUserPlugins';
 
 mongoose.set('autoCreate', false);
 mongoose.set('autoIndex', false);
 
 import { driver } from '@datastax/astra-mongoose';
 
-if (process.env.DATA_API_TABLES) {
-  // Mongoose Studio relies on countDocuments, estimatedDocumentCount, and findOneAndUpdate.
-  // for showing count results and updating documents. So hack around those
-  // for tables.
-  mongoose.plugin(schema => {
-    schema.pre(['countDocuments', 'estimatedDocumentCount'], async function (this: any) {
-      throw mongoose.skipMiddlewareFunction(
-        await this.model
-          .find(this.getFilter())
-          .select({ _id: 1 })
-          .then((res: any) => res.length)
-      );
-    });
+const configuredMongoose = mongoose.setDriver(driver);
+setupMongooseStudioUserModels(configuredMongoose);
 
-    schema.pre('updateOne', async function () {
-      const update = this.getUpdate();
-      if (!update || Array.isArray(update) || !update.$setOnInsert) {
-        return;
-      }
-      // $setOnInsert not supported in table mode
-      delete update.$setOnInsert;
-      this.setUpdate(update);
-    });
-
-    schema.pre('findOneAndUpdate', async function () {
-      throw mongoose.skipMiddlewareFunction(
-        await this.model.updateOne(
-          this.getFilter(),
-          this.getUpdate() ?? {}
-        ).then(() => this.model.findOne(this.getFilter()))
-      );
-    })
-  });
-}
-
-export default mongoose.setDriver(driver);
+export default configuredMongoose;
---
+++ typescript-express-reviews/src/setupMongooseStudioUserPlugins.ts
@@ -0,0 +1,39 @@
+import type { AstraMongoose } from '@datastax/astra-mongoose';
+
+export default function setupMongooseStudioUserPlugins(mongoose: AstraMongoose) {
+  if (!process.env.DATA_API_TABLES) {
+    return;
+  }
+
+  // Mongoose Studio relies on countDocuments, estimatedDocumentCount, and findOneAndUpdate
+  // for showing count results and updating documents. So hack around those for tables.
+  mongoose.plugin(schema => {
+    schema.pre(['countDocuments', 'estimatedDocumentCount'], async function (this: any) {
+      throw mongoose.skipMiddlewareFunction(
+        await this.model
+          .find(this.getFilter())
+          .select({ _id: 1 })
+          .then((res: any) => res.length)
+      );
+    });
+
+    schema.pre('updateOne', async function () {
+      const update = this.getUpdate();
+      if (!update || Array.isArray(update) || !update.$setOnInsert) {
+        return;
+      }
+      // $setOnInsert not supported in table mode
+      delete update.$setOnInsert;
+      this.setUpdate(update);
+    });
+
+    schema.pre('findOneAndUpdate', async function () {
+      throw mongoose.skipMiddlewareFunction(
+        await this.model.updateOne(
+          this.getFilter(),
+          this.getUpdate() ?? {}
+        ).then(() => this.model.findOne(this.getFilter()))
+      );
+    });
+  });
+}
